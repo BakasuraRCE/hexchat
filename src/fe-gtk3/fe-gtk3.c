e91d84605463040f8b62cbf0d2ea3adc68ee0238
Nahuel wexd <nahual_gomca@outlook.com.ar>
2019-05-29 21:54:10 -0300

    Created base for the new frontend

---
+++ src/fe-gtk3/fe-gtk3.c
@@ -0,0 +1,561 @@
+/* HexChat
+ * Copyright (C) 2019 Nahuel Gomez Castro
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <gtk/gtk.h>
+
+#include "../common/fe.h"
+#include "app.h"
+
+void
+fe_new_window (struct session *sess, int focus)
+{
+	
+}
+
+void
+fe_print_text (struct session *sess, char *text, time_t stamp,
+			   gboolean no_activity)
+{
+	
+}
+
+void
+fe_timeout_remove (int tag)
+{
+
+}
+
+int
+fe_timeout_add (int interval, void *callback, void *userdata)
+{
+	
+}
+
+int
+fe_timeout_add_seconds (int interval, void *callback, void *userdata)
+{
+	
+}
+
+void
+fe_input_remove (int tag)
+{
+	
+}
+
+int
+fe_input_add (int sok, int flags, void *func, void *data)
+{
+	
+}
+
+int
+fe_args (int argc, char *argv[])
+{
+	
+}
+
+void
+fe_init (void)
+{
+
+}
+
+void
+fe_main (void)
+{
+
+}
+
+void
+fe_exit (void)
+{
+
+}
+
+void
+fe_new_server (struct server *serv)
+{
+
+}
+
+void
+fe_message (char *msg, int flags)
+{
+
+}
+
+void
+fe_close_window (struct session *sess)
+{
+
+}
+
+void
+fe_beep (session *sess)
+{
+
+}
+
+void
+fe_add_rawlog (struct server *serv, char *text, int len, int outbound)
+{
+
+}
+
+void
+fe_set_topic (struct session *sess, char *topic, char *stripped_topic)
+{
+
+}
+
+void
+fe_cleanup (void)
+{
+
+}
+
+void
+fe_set_tab_color (struct session *sess, int col)
+{
+
+}
+
+void
+fe_update_mode_buttons (struct session *sess, char mode, char sign)
+{
+
+}
+
+void
+fe_update_channel_key (struct session *sess)
+{
+
+}
+
+void
+fe_update_channel_limit (struct session *sess)
+{
+
+}
+
+int
+fe_is_chanwindow (struct server *serv)
+{
+	return 0;
+}
+
+void
+fe_add_chan_list (struct server *serv, char *chan, char *users, char *topic)
+{
+
+}
+
+void
+fe_chan_list_end (struct server *serv)
+{
+
+}
+
+gboolean
+fe_add_ban_list (struct session *sess, char *mask, char *who, char *when, int rplcode)
+{
+	return 0;
+}
+
+gboolean
+fe_ban_list_end (struct session *sess, int rplcode)
+{
+	return 0;
+}
+
+void
+fe_notify_update (char *name)
+{
+
+}
+
+void
+fe_notify_ask (char *name, char *networks)
+{
+
+}
+
+void
+fe_text_clear (struct session *sess, int lines)
+{
+
+}
+
+void
+fe_progressbar_start (struct session *sess)
+{
+
+}
+
+void
+fe_progressbar_end (struct server *serv)
+{
+
+}
+
+void
+fe_userlist_insert (struct session *sess, struct User *newuser, gboolean sel)
+{
+
+}
+
+int
+fe_userlist_remove (struct session *sess, struct User *user)
+{
+	return 0;
+}
+
+void
+fe_userlist_rehash (struct session *sess, struct User *user)
+{
+
+}
+
+void
+fe_userlist_numbers (struct session *sess)
+{
+
+}
+
+void
+fe_userlist_clear (struct session *sess)
+{
+
+}
+
+void
+fe_userlist_set_selected (struct session *sess)
+{
+
+}
+
+void
+fe_dcc_add (struct DCC *dcc)
+{
+
+}
+
+void
+fe_dcc_update (struct DCC *dcc)
+{
+
+}
+
+void
+fe_dcc_remove (struct DCC *dcc)
+{
+
+}
+
+void
+fe_clear_channel (struct session *sess)
+{
+
+}
+
+void
+fe_session_callback (struct session *sess)
+{
+
+}
+
+void
+fe_server_callback (struct server *serv)
+{
+
+}
+
+void
+fe_url_add (const char *text)
+{
+
+}
+
+void
+fe_pluginlist_update (void)
+{
+
+}
+
+void
+fe_buttons_update (struct session *sess)
+{
+
+}
+
+void
+fe_dlgbuttons_update (struct session *sess)
+{
+
+}
+
+void
+fe_dcc_send_filereq (struct session *sess, char *nick, int maxcps, int passive)
+{
+
+}
+
+void
+fe_set_channel (struct session *sess)
+{
+
+}
+
+void
+fe_set_title (struct session *sess)
+{
+
+}
+
+void
+fe_set_nonchannel (struct session *sess, int state)
+{
+
+}
+
+void
+fe_set_nick (struct server *serv, char *newnick)
+{
+
+}
+
+void
+fe_change_nick (struct server *serv, char *nick, char *newnick)
+{
+
+}
+
+void
+fe_ignore_update (int level)
+{
+
+}
+
+int
+fe_dcc_open_recv_win (int passive)
+{
+	return FALSE;
+}
+
+int
+fe_dcc_open_send_win (int passive)
+{
+	return FALSE;
+}
+
+int
+fe_dcc_open_chat_win (int passive)
+{
+	return FALSE;
+}
+
+void
+fe_userlist_hide (session * sess)
+{
+
+}
+
+void
+fe_lastlog (session *sess, session *lastlog_sess, char *sstr, gtk_xtext_search_flags flags)
+{
+
+}
+
+void
+fe_set_lag (server * serv, long lag)
+{
+
+}
+
+void
+fe_set_throttle (server * serv)
+{
+	
+}
+
+void
+fe_set_away (server *serv)
+{
+
+}
+
+void
+fe_serverlist_open (session *sess)
+{
+
+}
+
+void
+fe_get_bool (char *title, char *prompt, void *callback, void *userdata)
+{
+
+}
+
+void
+fe_get_str (char *prompt, char *def, void *callback, void *ud)
+{
+
+}
+
+void
+fe_get_int (char *prompt, int def, void *callback, void *ud)
+{
+
+}
+
+void
+fe_idle_add (void *func, void *data)
+{
+	g_idle_add (func, data);
+}
+
+void
+fe_ctrl_gui (session *sess, fe_gui_action action, int arg)
+{
+
+}
+
+int
+fe_gui_info (session *sess, int info_type)
+{
+	return -1;
+}
+
+void *
+fe_gui_info_ptr (session *sess, int info_type)
+{
+	return NULL;
+}
+
+void fe_confirm (const char *message, void (*yesproc)(void *), void (*noproc)(void *), void *ud)
+{
+
+}
+
+char *fe_get_inputbox_contents (struct session *sess)
+{
+	return NULL;
+}
+
+void fe_set_inputbox_contents (struct session *sess, char *text)
+{
+
+}
+
+int fe_get_inputbox_cursor (struct session *sess)
+{
+	return 0;
+}
+
+void fe_set_inputbox_cursor (struct session *sess, int delta, int pos)
+{
+
+}
+
+void fe_open_url (const char *url)
+{
+
+}
+
+void fe_menu_del (menu_entry *me)
+{
+
+}
+
+char *fe_menu_add (menu_entry *me)
+{
+	return NULL;
+}
+
+void fe_menu_update (menu_entry *me)
+{
+	
+}
+
+void fe_uselect (struct session *sess, char *word[], int do_clear, int scroll_to)
+{
+
+}
+
+void
+fe_server_event (server *serv, int type, int arg)
+{
+
+}
+
+void
+fe_flash_window (struct session *sess)
+{
+
+}
+
+void
+fe_get_file (const char *title, char *initial,
+			 void (*callback) (void *userdata, char *file), void *userdata,
+			 int flags)
+{
+
+}
+
+void
+fe_tray_set_flash (const char *filename1, const char *filename2, int timeout)
+{
+
+}
+
+void
+fe_tray_set_file (const char *filename)
+{
+	
+}
+
+void
+fe_tray_set_icon (feicon icon)
+{
+
+}
+
+void
+fe_tray_set_tooltip (const char *text)
+{
+
+}
+
+void
+fe_userlist_update (session *sess, struct User *user)
+{
+
+}
+
+void
+fe_open_chan_list (server *serv, char *filter, int do_refresh)
+{
+
+}
+
+const char *
+fe_get_default_font (void)
+{
+	return NULL;
+}
